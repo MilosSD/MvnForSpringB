ceb71697aa6f14e86b0f19d0495c0f26e3158a10
Miloš Pirivatrić <pirisd@gmail.com>
2023-07-29 12:22:45 +0200

    Kreiran SpringBoot projekat.

---
+++ src/main/java/guru/springframework/mvnforspringb/MvnForSpringBApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mvnforspringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnForSpringBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnForSpringBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/mvnforspringb/MvnForSpringBApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mvnforspringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnForSpringBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
